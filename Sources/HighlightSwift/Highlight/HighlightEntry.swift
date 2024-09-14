71a118a2c84f4678ac4b939fa812de7e4f0af1ef
João Reichert <joaoreichert94@gmail.com>
2024-09-14 08:30:19 -0300

    Update macos EnvironmentValues

--- Sources/HighlightSwift/Highlight/HighlightEntry.swift
+++ Sources/HighlightSwift/Highlight/HighlightEntry.swift
@@ -1,5 +1,18 @@
 import SwiftUI
 
+#if os(macOS)
+struct HighlightKey: EnvironmentKey {
+    static let defaultValue: Highlight = Highlight()
+}
+
+extension EnvironmentValues {
+    var highlight: Highlight {
+        get { self[HighlightKey.self] }
+        set { self[HighlightKey.self] = newValue }
+    }
+}
+#elseif os(iOS)
 extension EnvironmentValues {
     @Entry var highlight: Highlight = Highlight()
 }
+#endif
